005ef7998a10dedcf7d759d65939ad1deaeea475
AlexJmzc <73499509+AlexJmzc@users.noreply.github.com>
2023-06-12 11:13:13 -0500

    Facial tracking en tiempo real

---
+++ FacialTracking/main.py
@@ -0,0 +1,25 @@
+# Código que utiliza un modelo entrenado en cascada para hacer un tracking de rostros en cámara
+
+# Instalar cv2 con el comando -> pip install cv2
+
+# Para cerrar la ventana de la cámara se presiona la tecla Esc
+
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')  
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, img = cap.read() 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for(x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        
+    cv2.imshow('img', img)
+       
+    k = cv2.waitKey(30)
+    if k == 27:
+        break
+cap.release()
